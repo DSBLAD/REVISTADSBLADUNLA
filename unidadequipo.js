60f72b1e023d02fc5995b8d6f9ecb1c1269897b8
melisabeatriz <melisabeatriz@gmail.com>
2025-10-10 16:19:08 -0300

    optimización imágenes equipo

--- unidadequipo.js
+++ unidadequipo.js
@@ -5,7 +5,7 @@ const miembros = [
     bio: "Clara Tapia es Doctora en Artes, y Diseñadora Industrial (UNLP). Ganadora de la Beca de Estancia Corta de investigación doctoral BEC.AR en el Politécnico de Milano. Donde estudió en el Doctorado de Investigación en Diseño POLIMI y trabajó en el laboratorio de fabricación digital Polifactory. Sus proyectos cruzan el diseño, la industria textil y el arte. Es Profesora Adjunta de Historia y Metodología del Diseño (UNLa) y ayudante en Historia del Diseño Industrial (UNLP). Como investigadora trabaja en programas de I+D para promover la innovación en la economía social. Es performer y diseñadora de vestuario en varios colectivos de artistas de la ciudad de La Plata.",
     institucion: "UNLa, UNLP, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "clara.jpeg",
+    foto: "clara",
   },
   {
     nombre: "Esp. Estefania Fondevila Sancet",
@@ -13,7 +13,7 @@ const miembros = [
     bio: "Estefanía Fondevila Sancet, es Especialista en Metodología de la Investigación Científica UNLa, Diplomada en Ciencia y Tecnología UNQ y programadora web, Responsable de la coordinacion de Investigación del Departamento de Humanidades y Artes de la UNLa, fundadora del HUB de Diseño Industrial de la UNLa, desarrolla herramientas de planificación, diagnóstico, desarrollo y seguimiento de proyectos y emprendimientos. Articula con empresas, instituciones y equipos interdisciplinarios. Se desempeña como Directora de proyectos de investigación, de becarios y pasantes, con instituciones nacionales e internacionales. Actualmente su actividad docente se desarrolla en UNLa, como profesora adjunta concursada en la materia Ciencia aplicada al Diseño Industrial, Taller de Prácticas Preprofesionales y en Taller Integrador Final en la Licenciatura en Diseño Industrial. Participa de redes nacionales de Gestión de la Ciencia y la Tecnología. 1er premio innovar 2012 y 2016, Distinguida con Diploma de Honor en el concurso Arte, ciencia, tecnologia e innovación",
     institucion: "UNLa, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "estefi.jpg",
+    foto: "estefi",
   },
   {
     nombre: "DG. Diana Corvalán",
@@ -21,7 +21,7 @@ const miembros = [
     bio: "Diana G. Corvalán es Diseñadora gráfica egresada de FADU, UBA, tiene estudios de posgrado en Comunicación y Creación Cultural, en el instituto Walter Benjamin en convenio con CAECE. Realizó la carrera docente especializada en arquitectura, diseño y urbanismo en FADU UBA. Se desempeñó como docente en diferentes universidades, donde también coordinó proyectos de cooperación y de extensión con instituciones nacionales e internacionales. En la actualidad su actividad docente se concentra en UNLa, es coordinadora académica de la Licenciatura en Diseño y Comunicación Visual y es profesora asociada concursada en la materia Espacio Tipográfico 1 en DyCV y en la materia Imagen, tipografía e identidad cultural en la Licenciatura en Audiovisión. Participa de equipos de investigación, redes y grupos de estudio vinculados con la tipografía, la caligrafía, el libro y la cultura gráfica. Siempre interesada por el universo de la imagen y el diseño en general, ávida lectora, le interesan las diferentes dimensiones del mundo",
     institucion: "UNLa, UBA, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "diana.jpeg",
+    foto: "diana",
   },
   {
     nombre: "Mg. DI Guillermo Andrade",
@@ -29,7 +29,7 @@ const miembros = [
     bio: "Guillermo Andrade es Diseñador Industrial y Magister en Marketing Internacional. Actualmente se encuentra en la realización de su tesis de Doctorado en Artes. Se desempeña como Director del posgrado 'Especialización en Tecnologías de Fabricación Digital' y Director de la carrera 'Licenciatura en Diseño Industrial' en la Universidad Nacional de Lanús. Investigador categoría II del sistema de incentivos de la Nación. Profesor Titular de la Universidad Nacional de La Plata y Profesor Asociado de la Universidad Nacional de Lanús. Miembro del Consejo Departamental del Departamento de Humanidades y Artes de la UNLa. Evaluador externo de proyectos de Ciencia, Tecnología e Innovación Productiva (Agencia Nacional de Promoción Científica y Tecnológica, Consejo Interuniversitario Nacional).",
     institucion: "UNLa, UNLP, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "guillermo-andrade.jpg",
+    foto: "guillermo-andrade",
   },
   {
     nombre: "Mg. DG Andrea Gergich",
@@ -37,7 +37,7 @@ const miembros = [
     bio: "Andrea Gergich es Diseñadora Gráfica, Magister en Diseño Comunicacional (FADU, UBA) y doctoranda en Historia del Arte (EIDAES-UNSAM). Docente universitaria e investigadora especializada en historia del diseño gráfico en Argentina y cultura gráfica local. Coordinadora Académica de la Licenciatura en Diseño y Comunicación Visual (UNLa) y Coordinadora técnica DyCV en el LaD-Laboratorio de Diseño (UNLa). Titular de Historia Social del Diseño y Coordinadora del Taller de Trabajo Final Integrador. Co-titular del Seminario de grado Historia del diseño en Argentina, y docente concursada en Historia de la Comunicación Visual en la FADU-UBA. Investigadora y directora de proyectos Amílcar Herrera (UNLa), UBACyT y PIA (FADU, UBA), Mecenazgo (CABA), e investigadora externa (CIAP-UNSAM-CONICET). Autora de artículos y capítulos de libros, disertante en congresos nacionales e internacionales. Fue Coordinadora del equipo de Diseño del Campus Virtual UNLa (2008-2020) y Directora de Estudios en Fundación Gutenberg (2001-2018).",
     institucion: "UNLa, UBA, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "andrea-gergich.jpg",
+    foto: "andrea-gergich",
   },
   {
     nombre: "Lic. en DI Emilia Di Nuzzo",
@@ -45,7 +45,7 @@ const miembros = [
     bio: "María Emilia Di Nuzzo es Diseñadora Industrial UNLa y maestranda en Metodologías de la Investigación Científica en la misma institución. Posee un posgrado en Diseño de Juegos, Juguetes e Interfaces Lúdicas FADU-UBA. Su perfil académico se ha centrado en el diseño inclusivo y en el estudio y aplicación de metodologías de diseño. Desde 2021 se desempeña como docente en las materias Métodos de Diseño, Semiótica Aplicada al Diseño e Historia Social del Diseño I y II. Actualmente, su campo de trabajo se orienta al desarrollo de soluciones proyectuales basadas en metodologías de diseño, dentro de una red de Talleres Protegidos del Área Metropolitana de Buenos Aires (AMBA), con foco en la mejora de procesos y entornos inclusivos.",
     institucion: "UNLa, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "emilia.jpg",
+    foto: "emilia",
   },
   {
     nombre: "Lic. en DCV Andrea Baffigi",
@@ -53,7 +53,7 @@ const miembros = [
     bio: "Andrea Soledad Baffigi, Licenciada en Diseño y Comunicación Visual graduada en la UNLa. Desde el 2017 se desempeña como docente investigadora en dicha institución educativa, actualmente formando parte la cátedra de Espacio Tipográfico 1 y Taller de Diseño Orientado. Además, forma parte del equipo docente de Tramo Inicial a la carrera y coopera en la organización y gestión de los Talleres Finales Integradores y el Laboratorio de Diseño. En 2024 se incorporó en la Diplomatura en Artes del Libro (UNA) como docente de las Clínicas de Diseño de proyectos editoriales. Fuera del ámbito académico, es diseñadora freelance.",
     institucion: "UNLa, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "andrea.jpeg",
+    foto: "andrea",
   },
   {
     nombre: "DT Cecilia Dorado",
@@ -61,7 +61,7 @@ const miembros = [
     bio: "Cecilia Dorado es Diseñadora Textil FADU UBA. Especialista en Gestión de la Tecnología y la Innovación (UNSAM) y Diplomada en Desarrollo Local y Economía Social (FLACSO). Docente adjunta e investigadora de la Licenciatura en Diseño Industrial y Coordinadora General del LAD Laboratorio de Diseño (UNLa). Investiga en diseño textil, innovación sustentable y biotextiles. Diseñadora en el Centro de Diseño Industrial del Instituto Nacional de Tecnología Industrial (INTI). Responsable del Biofablab y de la línea 'Materiales, procesos y productos innovadores biobasados'.",
     institucion: "UNLa, INTI, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "dorado.png",
+    foto: "dorado",
   },
   {
     nombre: "Esp. DI Diego Velazco",
@@ -69,7 +69,7 @@ const miembros = [
     bio: "Diego Velazco es Diseñador Industrial (UNLP) y Especialista en Metodología de la Investigación Científica (UNLa). Coordinador académico de la Licenciatura en Diseño Industrial (UNLa) y Coordinador Técnico del Laboratorio de Diseño (LAD-UNLA). Profesor adjunto concursado de Fundamentos de la Forma (UNLP) y de Modelado Digital, Taller de Diseño Industrial V y Taller de Trabajo Integrador Final (UNLa). Docente de Posgrado en la Especialización en Tecnologías de Fabricación Digital (UNLa). Director y codirector de proyectos Amílcar Herrera (UNLa), PAD (MINCyT), PROCODAS (MINCyT) y PIO (CONICET). Evaluador de la Agencia Nacional de Promoción de la Investigación, el Desarrollo Tecnológico y la Innovación. Profesional independiente en diseño y producción de mobiliario y capacitador en modelado digital.",
     institucion: "UNLa, UNLP, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "diego-velazco.jpg",
+    foto: "diego-velazco",
   },
   {
     nombre: "Mg. DI Edgardo Chanquia",
@@ -77,7 +77,7 @@ const miembros = [
     bio: "Edgardo Rafael Chanquia, es Magister en Metodología de la Investigación Científica UNLa. Posee los títulos de Diseñador Industrial UNLP, Prof. en Diseño Industrial UNLP y Diseñador Gráfico ESAV. Cuenta con amplia experiencia previa en investigación académica. Actualmente en la UNLa se desempeña como profesor adjunto concursado en las asignaturas: Dibujo Técnico; Morfología; Desarrollos Visuales; Taller de Diseño Industrial 1 y 3. Además, es docente del Seminario en Diseño de Paneles de Presentación de Productos y está a cargo de la organización, montaje y publicación de muestras y productos de los estudiantes de la Licenciatura en Diseño Industrial. Es Miembro del Consejo Departamental del Departamento de Humanidades y Artes de la UNLa.",
     institucion: "UNLa, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "chanqui.jpeg",
+    foto: "chanqui",
   },
   {
     nombre: "Esp. DG Angelina Sanchez",
@@ -85,7 +85,7 @@ const miembros = [
     bio: "Angelina Sánchez es diseñadora gráfica y especialista en diseño de tipografías (FADU-UBA). Desde 2005 dicta clases en la Universidad Nacional de Lanús (UNLa) en materias vinculadas al diseño editorial y uso tipográfico. Trabaja en forma independiente en proyectos de identidad visual, difusión y diseño editorial asociados al ámbito de la cultura y la música independiente. Participó en proyectos colaborativos en diseño, investigación y difusión como «Soytutype» fundidiora tipográfica; «AEZpress» letterpress; «Epígrafes porteños» tipografía en fachadas de Bs. As.; «Etnotipografías» proyecto de diseño en UNLa.; «Birras y letras» y «Chancho-va» colectivos de diseño tipográfico y divulgación. Algunos de sus diseños fueron seleccionados en la Bienal Latinoamericana «Tipos Latinos» y en la Bienal de Diseño FADU. «Ruda», tipografía publicada en Google Fonts recibió en 2023 la distinción del Sello Buen Diseño argentino y es utilizada actualmente en la identidad visual de la Escuela de Bellas Artes (ESAD) de Reims en Francia.",
     institucion: "UNLa, UBA, Argentina",
     contacto: "dsbladunla@gmail.com",
-    foto: "angelina.jpeg",
+    foto: "angelina",
   },
   {
     nombre: "Lic. en DCV Melisa Cerda",
@@ -93,9 +93,8 @@ const miembros = [
     bio: "Melisa Beatriz Cerda es Licenciada en Diseño y Comunicación Visual graduada en la Universidad de Lanús, donde actualmente trabaja como docente e investigadora. Docente de las materias Espacio Tipográfico 1, Taller de Diseño 4 y Computación 1. Investigadora en el proyecto 'Cartografía formal de la letra en torno a la obra de Francisco Salamone durante la década 1930 en la Provincia de Buenos Aires'. Diseñadora gráfica, desarrolladora web y bordadora, le apasiona todo lo que tenga que ver con el mundo gráfico y poder compartirlo a través de la docencia.",
     institucion: "UNLa, Argentina",
     contacto: "dsblad.diseno@gmail.com",
-    foto: "melisa.jpeg",
+    foto: "melisa",
   },
-
   {
     nombre: "Dra. DI Edurne Battista",
     institucion: "INTA, UNDAV, Argentina",
@@ -176,7 +175,7 @@ miembros.forEach((m) => {
   contenedor.innerHTML += `
     <div class="miembro-equipo flex items-start p-4 gap-2 bg-bg">
     <div class="img-container">
-      <img src="images/equipo/${m.foto || "placeholder.png"}" alt="" />
+      <img src="images/equipo/${m.foto || "placeholder"}.png" alt="" />
       </div>
       <div class="flex flex-col justify-center">
         <h3 class="font-bold">${m.nombre || ""}</h3>
