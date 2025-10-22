2a26e881accd6a861ff8713885d5c9d916a5eca8
melisabeatriz <melisabeatriz@gmail.com>
2025-10-22 00:14:17 -0300

    agrego todas las últimas versiones

--- n1desagregado.js
+++ n1desagregado.js
@@ -5,7 +5,8 @@ const tipoA = [
     subtitulo:
       "desarrollo de un sensor de CO2 para el monitoreo de la calidad del aire en la UNLa",
     tipo: "A",
-    ruta: "01_andrade-seucoo",
+    rutaImg: "01_andrade-seucoo",
+    rutaPdf: "andrade-proyecto_seucoo",
   },
   {
     autor: "andrade, guillermo",
@@ -13,28 +14,32 @@ const tipoA = [
     subtitulo:
       "Edificio talleres. El diseño del espacio adaptado a una propuesta pedagógica",
     tipo: "A",
-    ruta: "02_andrade-polo",
+    rutaImg: "02_andrade-polo",
+    rutaPdf: "andrade-polo_tecnologico",
   },
   {
-    autor: "corvalán, diana",
+    autor: "corvalan, diana",
     titulo: "Las firmas metálicas en las obras de Francisco Salamone",
     subtitulo: "",
     tipo: "A",
-    ruta: "05_corvalan-salamone",
+    rutaImg: "05_corvalan-salamone",
+    rutaPdf: "corvalan-firmas_salamone",
   },
   {
     autor: "covas, sheila",
     titulo: "Diseñar para neuroconvivir",
     subtitulo: "Diseño accesible orientado a las neurodivergencias",
     tipo: "A",
-    ruta: "06_covas-neuroconvivir",
+    rutaImg: "06_covas-neuroconvivir",
+    rutaPdf: "covas-disenar_neuroconvivir",
   },
   {
     autor: "dorado, cecilia",
     titulo: "Innovación y sustentabilidad en diseño textil",
     subtitulo: "experiencias de investigación en biotextiles",
     tipo: "A",
-    ruta: "08_dorado-sustentabilidad",
+    rutaImg: "08_dorado-sustentabilidad",
+    rutaPdf: "dorado-innovacion_sustentabilidad",
   },
   {
     autor: "fondevila, estefanía",
@@ -43,7 +48,8 @@ const tipoA = [
     subtitulo:
       "El caso de las revistas del Departamento de Humanidades y Artes de la Universidad Nacional de Lanús",
     tipo: "A",
-    ruta: "09_fondevilla-etica",
+    rutaImg: "09_fondevilla-etica",
+    rutaPdf: "fondevila-ia_etica",
   },
   {
     autor: "gergich, andrea",
@@ -51,14 +57,16 @@ const tipoA = [
     subtitulo:
       "La revalorización de la memoria gráfica para una historia del diseño local",
     tipo: "A",
-    ruta: "11_gergich-patrimonio",
+    rutaImg: "11_gergich-patrimonio",
+    rutaPdf: "gergich-patrimonio_cultura",
   },
   {
     autor: "suarez, valeria",
     titulo: "El oficio de diseñadores en el mundo digital",
     subtitulo: "",
     tipo: "A",
-    ruta: "14_suarez-oficio",
+    rutaImg: "14_suarez-oficio",
+    rutaPdf: "suarez-oficio_disenadores",
   },
   {
     autor: "tapia, clara",
@@ -66,15 +74,8 @@ const tipoA = [
       "Diseño para el fortalecimiento identitario y productivo de la Cooperativa Textil La Esperanza",
     subtitulo: "",
     tipo: "A",
-    ruta: "15_tapia-cooperativa",
-  },
-  {
-    autor: "tapia, clara",
-    titulo: "Del diagnóstico a la gestión estratégica de diseño",
-    subtitulo:
-      "Experiencias didácticas en la materia Métodos del Diseño I durante el periodo 2019 - 2025 en la Universidad Nacional de Lanús",
-    tipo: "A",
-    ruta: "16_tapia-metodos",
+    rutaImg: "15_tapia-cooperativa",
+    rutaPdf: "tapia-cooperativa",
   },
   {
     autor: "weissel, marcelo",
@@ -82,7 +83,17 @@ const tipoA = [
     subtitulo:
       "Contexto y tipografía de las obras de Francisco Salamone en la Provincia de Buenos Aires",
     tipo: "A",
-    ruta: "18_weissel-salamone",
+    rutaImg: "18_weissel-salamone",
+    rutaPdf: "weissel-grito_urbano",
+  },
+  {
+    autor: "dorado & gergich",
+    titulo: "Cinco años del Laboratorio de Diseño de la UNLa",
+    subtitulo:
+      "Orígenes, ideas fundantes y proyectos clave de sus primeros cinco años",
+    tipo: "A",
+    rutaImg: "24_dorado-gergich_lad",
+    rutaPdf: "dorado-gergich_lad",
   },
 ];
 
@@ -92,7 +103,8 @@ const tipoB = [
     titulo: "Diseño, poética y bien común",
     subtitulo: "Entrevista a María Gemma Sánchez",
     tipo: "B",
-    ruta: "07_damico-poetica",
+    rutaImg: "07_damico-poetica",
+    rutaPdf: "damico-diseno_poetica",
   },
 ];
 
@@ -102,7 +114,8 @@ const tipoC = [
     titulo: "Rediseñar para circular",
     subtitulo: "formación proyectual en clave de economía circular",
     tipo: "C",
-    ruta: "03_caffaro-circular",
+    rutaImg: "03_caffaro-circular",
+    rutaPdf: "caffaro-rediseniar_circular",
   },
   {
     autor: "carpintero, carlos",
@@ -110,7 +123,8 @@ const tipoC = [
     subtitulo:
       "Estado de la cuestión y estrategias didácticas en el uso de IA para la enseñanza y aprendizaje del Diseño y la Comunicación Visual en la Universidad Nacional de Lanús",
     tipo: "C",
-    ruta: "04_carpintero-ia",
+    rutaImg: "04_carpintero-ia",
+    rutaPdf: "carpintero-revolucion_ia",
   },
   {
     autor: "fondevila, estefanía",
@@ -118,7 +132,8 @@ const tipoC = [
     subtitulo:
       "el rol de los proyectos de Tesinas de grado y prácticas profesionalizantes en la transferencia tecnológica",
     tipo: "C",
-    ruta: "10_fondevilla-semillero",
+    rutaImg: "10_fondevilla-semillero",
+    rutaPdf: "fondevila-universidades_semillero",
   },
   {
     autor: "ossorio domeq, mayte",
@@ -126,7 +141,8 @@ const tipoC = [
       "Paneles integradores: del análisis a la proyección en Textil II y III",
     subtitulo: "",
     tipo: "C",
-    ruta: "12_osorio-paneles",
+    rutaImg: "12_osorio-paneles",
+    rutaPdf: "ossorio-paneles_integradores",
   },
   {
     autor: "ruscitti, andrés",
@@ -134,7 +150,8 @@ const tipoC = [
       "Herramientas de la industria 4.0 en la formación tecnológica del diseño industrial",
     subtitulo: "",
     tipo: "C",
-    ruta: "13_ruscitti-industria",
+    rutaImg: "13_ruscitti-industria",
+    rutaPdf: "ruscitti-herramientas_industria",
   },
   {
     autor: "velazco, diego",
@@ -142,7 +159,8 @@ const tipoC = [
     subtitulo:
       "Su aplicación en la transición hacia el trabajo integrador final",
     tipo: "C",
-    ruta: "17_velazco-ia",
+    rutaImg: "17_velazco-ia",
+    rutaPdf: "velazco-ia_taller",
   },
   {
     autor: "moreira, leo",
@@ -150,14 +168,8 @@ const tipoC = [
     subtitulo:
       "Proyecto colectivo de celebración de los 20 años de la Carrera de Diseño y Comunicación Visual",
     tipo: "C",
-    ruta: "19_moreira-maraton",
-  },
-  {
-    autor: "sánchez, angelina",
-    titulo: "Artículo sobre el diseño de la revista DSB LAD, en confección",
-    subtitulo: "",
-    tipo: "C",
-    ruta: "20_sanchez-revista",
+    rutaImg: "19_moreira-maraton",
+    rutaPdf: "moreira-la_maraton",
   },
   {
     autor: "faye, juan",
@@ -165,14 +177,34 @@ const tipoC = [
     subtitulo:
       "Diseño industrial aplicado al cultivo de hongos para producción de baja escala y experimental",
     tipo: "C",
-    ruta: "22_faye-champ",
+    rutaImg: "22_faye-champ",
+    rutaPdf: "faye-champ",
   },
   {
-    autor: "thomas y junco",
+    autor: "thomas & junco",
     titulo: "Experiencias estudiantes UNLa",
     subtitulo: "",
     tipo: "C",
-    ruta: "23_junco-thomas-experiencias",
+    rutaImg: "23_junco-thomas-experiencias",
+    rutaPdf: "junco-thomas_experiencias",
+  },
+  {
+    autor: "estudiantes dycv",
+    titulo: "Diseño en Red",
+    subtitulo:
+      "Identidad visual del II Encuentro de la Red Latinoamericana de Cultura Gráfica 2025",
+    tipo: "C",
+    rutaImg: "25_estudiantes-cultura",
+    rutaPdf: "estudiantes-red_cultura",
+  },
+  {
+    autor: "tapia, clara",
+    titulo: "Del diagnóstico a la gestión estratégica de diseño",
+    subtitulo:
+      "Experiencias didácticas en la materia Métodos del Diseño I durante el periodo 2019 - 2025 en la Universidad Nacional de Lanús",
+    tipo: "C",
+    rutaImg: "16_tapia-metodos",
+    rutaPdf: "tapia-diagnostico",
   },
 ];
 
@@ -182,8 +214,33 @@ const tipoD = [
     titulo: "Editorial",
     subtitulo: "",
     tipo: "D",
-    ruta: "21_bozzani-editorial",
+    rutaImg: "21_bozzani-editorial",
+    rutaPdf: "bozzani-editorial",
   },
+  {
+    autor: "recalde, aritz",
+    titulo: "Editorial",
+    subtitulo: "",
+    tipo: "D",
+    rutaImg: "26_recalde_editorial",
+    rutaPdf: "recalde-editorial",
+  },
+  // {
+  //   autor: "",
+  //   titulo: "Dossier",
+  //   subtitulo: "5 años del LAD: Presentación de proyectos",
+  //   tipo: "D",
+  //   rutaImg: "dossier",
+  //   rutaPdf: "dossier-5_aniversario_LAD",
+  // },
+  // {
+  //   autor: "",
+  //   titulo: "Infografía",
+  //   subtitulo: "5 años del LAD: Visualización de proyectos",
+  //   tipo: "D",
+  //   rutaImg: "infografia",
+  //   rutaPdf: "infografia-5_aniversario_LAD",
+  // },
 ];
 
 const contenedorArticulos = document.getElementById("contenedor-articulos");
@@ -191,7 +248,7 @@ const contenedorArticulos = document.getElementById("contenedor-articulos");
 function renderPublicaciones({
   data = [],
   contenedorId,
-  carpetaDocs = "./documents/n01/desagregado/",
+  carpetaDocs = "./documents/n01/desagregado/DSBLAD01-2025_",
   carpetaImgs = "./images/n1-desagregado-img/",
   placeholderImg = "./images/n1-desagregado-img/placeholder.png",
   numeroEdicion = "DSB LAD #1",
@@ -206,7 +263,9 @@ function renderPublicaciones({
     const autor = item?.autor || "";
     const titulo = item?.titulo || "";
     const subtitulo = item?.subtitulo || "";
-    const enlaceDescarga = item?.ruta ? `${carpetaDocs}${item.ruta}.pdf` : "";
+    const enlaceDescarga = item?.rutaPdf
+      ? `${carpetaDocs}${item.rutaPdf}.pdf`
+      : "";
 
     card.innerHTML = `
       <div class="img-container"></div>
@@ -221,7 +280,7 @@ function renderPublicaciones({
 
     // Imagen con fallback
     const imagen = document.createElement("img");
-    imagen.src = `${carpetaImgs}${item.ruta}.png`;
+    imagen.src = `${carpetaImgs}${item.rutaImg}.png`;
     imagen.alt = titulo;
     imagen.onerror = () => {
       imagen.src = placeholderImg;
